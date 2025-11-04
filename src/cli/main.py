feb36482890df7bb1ddb4cda5b328891850b12d8
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2025-11-04 15:28:48 +0100

    added module class, construtor and method placeholders

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,4 +1,36 @@
-#Full Calculator App Project
+# Full Calculator App Project
 
-#Libs
+# Libs
 
+
+class calc_app:
+    def __init__(self) -> None:
+        pass
+
+    def valid_symbols(self):
+        SYMBOLS = ("+", "-", "*", "/", "^", "%")
+        return SYMBOLS
+
+    # Addition method
+    def add(self):
+        pass
+
+    # Subtraction method
+    def sub(self):
+        pass
+
+    # Multiplication method
+    def mul(self):
+        pass
+
+    # Division method
+    def div(self):
+        pass
+
+    # Exponentiation method
+    def expo(self):
+        pass
+
+    # Modulus method
+    def mod(self):
+        pass
