ead30560393905c41662378d6d9c22c20afaf961
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2025-11-04 21:21:10 +0100

    implemented a fucntion to validate user input, requiring the user to input an actual value of integer type and a valid symbol from the list of symbols

--- py_calculator_app/src/cli/main.py
+++ py_calculator_app/src/cli/main.py
@@ -7,31 +7,70 @@ class calc_app:
     SYMBOLS = ("+", "-", "*", "/", "^", "%")
 
     def __init__(self) -> None:
-        self.get_user_input()
-        pass
-
-    def get_user_input(self):
         prompt = "What do you want to calculate\n:  "
         user_input = input(prompt)
         self.validate_input(user_input)
-        pass
+
+    # def get_user_input(self):
+    #     prompt = "What do you want to calculate\n:  "
+    #     user_input = input(prompt)
+    #     self.validate_input(user_input)
+    #     pass
 
     def validate_input(self, input: str):
         input = input.strip()
 
-        try:
-            symbol_list = []
+        operands = []
+        operators = []
 
-            for _ in self.SYMBOLS:
-                if _ in input:
-                    symbol_list.append(_)
+        for _ in input.split():
+            if not _.isdigit() and _ not in self.SYMBOLS:
+                raise ValueError(f"This is invalid. '{_}'")
 
-            print(symbol_list)
-            # if input.isdigit():
-            #     pass
-        except:
-            print(f"{_} isn't a valid operator")
-        pass
+        for _ in self.SYMBOLS:
+            if _ in input:
+                operators.append(_)
+
+        for _ in input.split():
+            if _.isdigit():
+                operands.append(_)
+
+        print(operands)
+        print(operators)
+        #     if self.SYMBOLS not in input.split():
+        #         print("Yes")
+        #     else:
+        #         print("No")
+        # # print(input.split())
+        # print(len(input.split()))
+
+        # for _ in (input):
+        #     print(f"{_}")
+
+        # if input.isdecimal():
+        #     print("decimal")
+        # if input.isnumeric():
+        #     print("numeric")
+        # if input.isdigit():
+        #     print("digit")
+        # if input.isalnum():
+        #     print("alphanum")
+        # if input.isalpha():
+        #     print("alpha")
+
+        # try:
+        #     symbol_list = []
+
+        #     for _ in self.SYMBOLS:
+        #         if _ in input:
+        #             symbol_list.append(_)
+
+        #     print(symbol_list)
+        #     # if input.isdigit():
+        #     #     pass
+        # except:
+        #     print(f"{_} isn't a valid operator")
+        # pass
 
     # Addition method
     def add(self):
@@ -59,6 +98,8 @@ class calc_app:
 
 
 def main():
+    print("App Starts...\n")
+    print("Welcome to the Basic Calculator Terminal App\n")
     calc_app()
 
 
